762c6a55fe028b8ef95f75d274da702bbadf9c3b
v0xa1tor <v0xa1tor.emz@gmail.com>
2026-04-03 22:58:12 -0300

    Add auto theme (dark and light)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  css: ["bootstrap/dist/css/bootstrap.min.css"]
+  css: ["bootstrap/dist/css/bootstrap.min.css"],
+  app: {
+    head: {
+      script: [{ src: '/scripts/auto-theme.js' }], // Dark & light theme
+    }
+  }
 });
