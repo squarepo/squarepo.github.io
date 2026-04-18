e8e38541c5db6d095e9142a4f5f8a251fb44ff64
v0xa1tor <v0xa1tor.emz@gmail.com>
2026-04-18 20:16:08 -0300

    Adjust breadcrumb scroll

--- app/components/Navbar.vue
+++ app/components/Navbar.vue
@@ -14,25 +14,28 @@ watch(
 </script>
 
 <template>
-  <nav class="navbar bg-body-tertiary">
-    <div class="px-3 w-100 d-flex align-items-center gap-3">
-      <button type="button" class="btn btn-outline-secondary fs-2 p-1" data-bs-toggle="offcanvas" data-bs-target="#offcanvas">
+  <nav class="navbar p-0 bg-body-tertiary">
+    <div class="w-100 h-100 d-flex align-items-center">
+      <button type="button" class="btn btn-outline-secondary fs-2 p-1 m-2" data-bs-toggle="offcanvas" data-bs-target="#offcanvas">
         <i class="bi bi-list"></i>
       </button>
-      <ol
-        v-if="pathEntries.length > 1"
-        class="breadcrumb m-0 d-flex flex-nowrap text-truncate w-100 overflow-auto"
-      >
-        <li
-          v-for="(entry, i) in pathEntries"
-          :key="`navbar-${entry.path}`"
-          class="breadcrumb-item flex-shrink-0"
-          :class="{ active: pathEntries.length === i + 1 }"
+      <div class="w-100 h-100 px-2 overflow-auto d-flex align-items-center">
+        <ol
+          v-if="pathEntries.length > 1"
+          class="breadcrumb m-0 d-flex flex-nowrap text-nowrap"
         >
-          <NuxtLink v-if="(i + 1) < pathEntries.length" class="text-decoration-none" :to="entry.path">{{ entry.name }}</NuxtLink>
-          <div v-else>{{ entry.name }}</div>
-        </li>
-      </ol>
+          <li
+            v-for="(entry, i) in pathEntries"
+            :key="`navbar-${entry.path}`"
+            class="breadcrumb-item flex-shrink-0"
+            :class="{ active: pathEntries.length === i + 1 }"
+          >
+            <NuxtLink v-if="(i + 1) < pathEntries.length" class="text-decoration-none" :to="entry.path">{{ entry.name }}</NuxtLink>
+            <div v-else class="d-inline">{{ entry.name }}</div>
+          </li>
+          <div class="ps-2"></div>
+        </ol>
+      </div>
     </div>
   </nav>
 </template>
