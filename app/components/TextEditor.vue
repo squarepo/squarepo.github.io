56d9fc819f146ae303f678b997cb8f80b9c3602f
v0xa1tor <v0xa1tor.emz@gmail.com>
2026-04-18 20:13:58 -0300

    Change file origin

--- app/components/TextEditor.vue
+++ app/components/TextEditor.vue
@@ -2,24 +2,26 @@
 import type { File } from '~/types/fs';
 
 const fsStore = useFsStore();
+const props = defineProps<{ file: File }>();
 const textareaEl = ref<HTMLTextAreaElement | null>(null);
 
 async function updateFileContent(newContent: string) {
   if (textareaEl.value) updateTextareaHeight(textareaEl.value);
-  if (fsStore.currentEntry) {
-    await fsStore.updateFileContent(fsStore.currentEntry.path, newContent);
-  }
+  await fsStore.updateFileContent(props.file.path, newContent);
 }
 
 function updateTextareaHeight(el: HTMLTextAreaElement) {
   el.style.height = "auto";
-  el.style.height = el.scrollHeight + "px";
+  el.style.height = el.scrollHeight - 10 + "px";
 }
 
 onMounted(() => {
   if (textareaEl.value) {
-    updateTextareaHeight(textareaEl.value);
     window.addEventListener("resize", () => updateTextareaHeight(textareaEl.value!));
+    watch(() => props.file, async () => {
+      await nextTick();
+      updateTextareaHeight(textareaEl.value!);
+    }, { immediate: true });
   }
 });
 </script>
@@ -31,7 +33,7 @@ onMounted(() => {
       class="form-control p-3 border-0 shadow-none overflow-hidden w-100"
       style="resize: none; box-sizing: border-box; padding-bottom: 200px !important;"
       placeholder="Texto..."
-      :value="(fsStore.currentEntry as File)?.content"
+      :value="props.file.content"
       @input="updateFileContent(($event.target as HTMLTextAreaElement).value)"
     ></textarea>
   </div>
